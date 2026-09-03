9aa71cf2b0b8d0ae5f60b0e370f2bb9c07cb9609
Cesar <22041150@itdurango.edu.mx>
2026-09-02 20:20:00 -0600

    Practica 04 Flask Jinja CSS PostgreSQL

---
+++ app.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template, request
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+DB_CONFIG = {
+    "host": os.environ.get("DB_HOST", "localhost"),
+    "port": os.environ.get("DB_PORT", "5432"),
+    "user": os.environ.get("DB_USER", "postgres"),
+    "password": os.environ.get("DB_PASSWORD", ""),
+    "dbname": os.environ.get("DB_NAME", "practica04_bd")
+}
+
+def conectar():
+    return psycopg2.connect(**DB_CONFIG)
+
+@app.route("/")
+def inicio():
+    return render_template("index.html")
+
+@app.route("/saludar", methods=["POST"])
+def f_saludar():
+    nombre = request.form["nombre"]
+    pasatiempos = request.form.getlist("pasatiempos")
+    me_gusta = request.form["me_gusta"]
+
+    pasatiempos_texto = ", ".join(pasatiempos)
+
+    conexion = conectar()
+    cursor = conexion.cursor()
+    cursor.execute("""
+        INSERT INTO alumnos (nombre, pasatiempos, me_gusta)
+        VALUES (%s, %s, %s)
+    """, (nombre, pasatiempos_texto, me_gusta))
+    conexion.commit()
+    cursor.close()
+    conexion.close()
+
+    return render_template(
+        "saludar.html",
+        nombre=nombre,
+        pasatiempos=pasatiempos,
+        me_gusta=me_gusta
+    )
+
+@app.route("/alumnos")
+def listar_alumnos():
+    conexion = conectar()
+    cursor = conexion.cursor()
+    cursor.execute("SELECT * FROM alumnos ORDER BY id")
+    alumnos = cursor.fetchall()
+    cursor.close()
+    conexion.close()
+
+    return render_template(
+        "listar_alumnos.html",
+        alumnos=alumnos
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
